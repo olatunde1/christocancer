b01099cc5e26419c500d7e68bed8568028d7dbfd
Adetunji <idowuolatunde32@gmail.com>
2023-10-16 18:16:32 +0100

    read more button fixed

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ let actionEL = document.getElementById('about');
 clickHandler.addEventListener('click', function () {
     actionEL.scrollIntoView(true);
 })
+
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
